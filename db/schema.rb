c90366a34b6e6ed3fbed4356c27135a48e592abb
Yoan Ante <yoanante@gmail.com>
2019-01-09 22:00:59 -0500

    Added migration to add password column to user schema changed on migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_185110) do
+ActiveRecord::Schema.define(version: 2019_01_10_020227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 2019_01_09_185110) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "company_id"
+    t.string "password"
   end
 
 end
